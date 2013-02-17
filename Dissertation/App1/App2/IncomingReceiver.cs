a175666123853afe12c90c5dc6c32a06a29be4c2
mmfraser <marc@coopersoftware.co.uk>
2013-02-17 13:34:12 +0000

    update

---
+++ Dissertation/App1/App2/IncomingReceiver.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+using Newtonsoft.Json;
+
+using ComputeAndroidSDK.Communication;
+
+namespace com.ComputeApps.TestApp {
+    [BroadcastReceiver(Exported=true, Enabled=true)]
+    [IntentFilter(new[] { "com.ComputeApps.TestApp.Intents.ReceiveResult" })]
+    public class IncomingReceiver : BroadcastReceiver {
+    
+        public override void OnReceive(Context context, Intent intent) {
+            try {
+                if (intent.Action == "com.ComputeApps.TestApp.Intents.ReceiveResult") {
+
+                }
+
+                //CommPackage cp = CommPackage.DeserializeJson(intent.GetStringExtra("CommPackage"));
+                //Log.Error("com.ComputeApps.TestApp.Intents.DoWork", "HERE!!");
+            
+
+                //ComputeApps.TestApp.WorkList.SetAppContext(context);
+                //ComputeApps.TestApp.WorkList.AddWorkItem(cp);
+
+                //int test = 0;
+            } catch (Exception ex) {
+                Log.Error("com.ComputeApps.TestApp.Intents.ReceiveResult", ex.Message);
+            }
+
+            
+
+          
+        }
+    }
+}
---
+++ Dissertation/SharedDevClasses/WorkOrder.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ComputeAndroidSDK.Communication {
+    public class WorkOrder {
+        public int WorkOrderId {
+            get;
+            set;
+        }
+        public int DeviceId {
+            get;
+            set;
+        }
+        public int ApplicationId {
+            get;
+            set;
+        }
+        public string CommPackageJson {
+            get;
+            set;
+        }
+        public System.DateTime ReceiveTime {
+            get;
+            set;
+        }
+        public Nullable<int> SlaveWorkerId {
+            get;
+            set;
+        }
+        public Nullable<System.DateTime> SlaveWorkerSubmit {
+            get;
+            set;
+        }
+        public Nullable<System.DateTime> SlaveWorkOrderLastCommunication {
+            get;
+            set;
+        }
+        public string WorkOrderStatus {
+            get;
+            set;
+        }
+        public string WorkOrderResultJson {
+            get;
+            set;
+        }
+        public Nullable<System.DateTime> StartComputationTime {
+            get;
+            set;
+        }
+        public Nullable<System.DateTime> EndComputationTime {
+            get;
+            set;
+        }
+
+    }
+}
